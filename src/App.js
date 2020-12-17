ee0f544fa92a914a4ab80f7600cc21d92fc958ca
Christian Close <ChristianClose55@gmail.com>
2020-12-17 14:03:37 -0600

    implemented firebase utils, included ability to store authenticated users into firebase database

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       currentUser: null
@@ -20,9 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentState:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => console.log(this.state))
+        });
+        console.log(this.state);
+      } else {
+        this.setState({currentUser: userAuth});
+      }
     });
   }
 
@@ -39,7 +52,7 @@ class App extends React.Component {
           <Route path="/shop" component={ShopPage} />
           <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
-  
+
       </div>
     );
   }
